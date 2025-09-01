defb148ebe323a0cd6a8d62109c84f00fe1c3c41
Fritzie Primananda Adi Praja <primanandafritzie@gmail.com>
2025-09-01 23:40:30 +0700

    changing route hot fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom"; 
 import Menu from "./components/Menu.jsx";
 import Sinonim from "./components/Sinonim";
 import Antonim from "./components/Antonim";
@@ -7,6 +7,7 @@ import Antonim from "./components/Antonim";
 export default function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/menu" replace />} />
       <Route path="/menu" element={<Menu />} />
       <Route path="/sinonim" element={<Sinonim />} />
       <Route path="/antonim" element={<Antonim />} />
